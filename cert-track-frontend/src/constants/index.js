2df748aa501955cd65a5dfd5aed00f32169fe9bb
Mickey03272 <132447170+Mickey03272@users.noreply.github.com>
2024-03-14 20:18:05 +0700

    Delete cert-track-frontend directory

--- cert-track-frontend/src/constants/index.js
+++
@@ -1,6 +0,0 @@
-// export const API_BASE_URL = 'http://localhost:8080/api';
-export const API_BASE_URL = import.meta.env.VITE_APP_API_BASE_URL 
-
-export const ACCESS_TOKEN = 'accessToken';
-
-export const COMPANY_NAME = "G-ABLE";
